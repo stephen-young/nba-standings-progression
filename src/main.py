24f96051d0247c483b92ab13508b4d3ad2eed740
Stephen Young <stephen.young.m@gmail.com>
2020-04-17 21:15:08 -0400

    Update function and module import

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 """MAIN.PY is a python script used for testing module functions"""
-import win_percent as wp
+import standings_progression as sp
 import matplotlib.pyplot as plt
 
 filename = "./standings_data/2017_east_standings_by_date.xlsx"
-ws_data = wp.get_standings_data_from_spreadsheet(filename)
-win_frac_data = wp.process_spreadsheet_data(ws_data)
+ws_data = sp.get_standings_data_from_spreadsheet(filename)
+win_frac_data = sp.process_standings_data(ws_data)
 
-fig = wp.plot_standings_progression(win_frac_data)
+fig = sp.plot_standings_progression(win_frac_data)
 plt.show()
