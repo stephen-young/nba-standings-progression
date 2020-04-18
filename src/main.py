a63058e8c8c159a3fe4c8df061ef0ea43bb6a52c
Stephen Young <stephen.young.m@gmail.com>
2020-04-17 21:05:14 -0400

    Streamline code using pandas library

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 """MAIN.PY is a python script used for testing module functions"""
 import win_percent as wp
-from pandas import date_range
+import matplotlib.pyplot as plt
 
-filename = "2019_east_standings_by_date.xlsx"
+filename = "./standings_data/2017_east_standings_by_date.xlsx"
 ws_data = wp.get_standings_data_from_spreadsheet(filename)
 win_frac_data = wp.process_spreadsheet_data(ws_data)
-wp.plot_win_fraction_by_date(win_frac_data)
 
+fig = wp.plot_standings_progression(win_frac_data)
+plt.show()
--- src/win_percent.py
+++ src/win_percent.py
@@ -1,110 +1,91 @@
-from openpyxl import load_workbook
-from datetime import datetime
-from parse import compile
-from pandas import DataFrame, date_range, DateOffset, read_excel, to_numeric, concat
-from constants import TEAM_COLOURS, PLOT
-
 import pandas
-import numpy
 import matplotlib.pyplot as plt
+from constants import TEAM_COLOURS, PLOT
 pandas.plotting.register_matplotlib_converters()
 
 def get_standings_data_from_spreadsheet(filename):
 
-  workbook = load_workbook(filename,read_only=True,data_only=True)
-  sheet = workbook.active
-  p = compile('{team} ({win:d}-{loss:d})')
-  standings_data = []
+  col_names = ['1st','2nd', '3rd', '4th', '5th','6th', '7th', '8th','9th', \
+    '10th', '11th', '12th', '13th', '14th', '15th']
 
-  for row in sheet.iter_rows(values_only=True):
+  standings_data = pandas.read_excel(filename, header=None, index_col=0, \
+    parse_dates=True, names=col_names)
 
-    date = datetime.strptime(row[0],"%b %d, %Y").date()
-    data = [{'team':'','win':0,'loss':0}]*(len(row)-1)
+  standings_data = standings_data.stack()
 
-    for i in range(1,len(row)):
-      item = row[i]
+  standings_pattern = r'(?P<team>[A-Z]{3})\s\((?P<win>\d+)\-(?P<loss>\d+)\)'
 
-      if item is not None:
-        r = p.search(item)
-        data[i-1] = r.named
+  standings_data = standings_data.str.extract(standings_pattern)
 
-    standing_record = {
-      "date": date,
-      "standing_data": data
-    }
+  standings_data['win'] = pandas.to_numeric(standings_data['win'])
+  standings_data['loss'] = pandas.to_numeric(standings_data['loss'])
+  standings_data['GP'] = standings_data['win'] + standings_data['loss']
+  standings_data['PCT'] = standings_data['win'] / standings_data['GP']
+  standings_data['rank'] = standings_data.index.levels[1][standings_data.index.codes[1]]
+  standings_data.index = standings_data.index.droplevel(1)
 
-    standings_data.append(standing_record)
+  return standings_data 
 
-  return standings_data
+def process_spreadsheet_data(standings_data):
 
-def process_spreadsheet_data(data):
+  end_date = standings_data.index[-1]
+  team_list = list(standings_data.loc[end_date,'team'])
 
-  team_list = [item['team'] for item in data[-1]['standing_data']]
+  win_frac_data = [standings_data[standings_data['team'] == team]['PCT'] \
+    for team in team_list]
 
-  dates = []
-  win_percent = {team: [] for team in team_list}
+  win_frac_df = pandas.concat(win_frac_data,axis=1,keys=team_list)
+  
+  return win_frac_df
 
-  for item in data:
-
-    dates.append(item['date'])
-    standing_data = item['standing_data']
-
-    for team in team_list:
-
-      team_data = [i for i in standing_data if i['team'] == team]
-
-      if len(team_data) == 1:
-        team_data = team_data[0]
-        win_percent[team].append(team_data['win'] / \
-          (team_data['win'] + team_data['loss']))
-      else:
-        win_percent[team].append(None)
-
-  # package data into a dataframe object
-
-  df = DataFrame(win_percent, index = dates)
-  return df
-
-def plot_win_fraction_by_date(data):
+def plot_standings_progression(data):
 
   # // put magic numbers into a dict in constants.py
 
-  col_labels = list(data.columns)
+  team_list = list(data.columns)
   start_date = data.index[0]
   end_date = data.index[-1]
+  dates = data.index.to_pydatetime()
 
   # // make x-ticks 1 month offsets from start date rather than the start of each month
-  date_ticks = date_range(start_date, end_date, freq=DateOffset(months=1))
+  date_ticks = pandas.date_range(start_date, end_date, freq=pandas.DateOffset(months=1))
 
   fig, axes = plt.subplots(figsize=PLOT['Figure']['Size'])
+  axes.plot(dates, data.values, linewidth=PLOT['Line']['Width'])
+
+  # format y-axis
   axes.set_ylabel('Win fraction (wins / games played to-date)')
   axes.set_ylim(PLOT['Axes']['YMin'], PLOT['Axes']['YMax'])
   axes.set_yticks(PLOT['Axes']['YTick'])
 
-  axes.set_xlabel('Date (YYYY-MM-DD)')
+  # format x-axis
+  axes.set_xlabel('Date')
   axes.set_xlim(start_date, end_date)
   axes.set_xticks(date_ticks)
 
+  # apply gird
   axes.grid()
-
-  for col in col_labels:
-      axes.plot(data.index, data[col], label=col, linewidth=PLOT['Line']['Width'])
 
   line_array = axes.get_lines()
 
-  # Set line, marker and marker edge colours based on team colours
-  for line in line_array:
-      team = line.get_label()
-      colours = TEAM_COLOURS[team]
-      line.set_color(colours['line'])
-      line.set_marker('.')
-      line.set_markerfacecolor(colours['marker'])
-      line.set_markeredgecolor(colours['edge'])
-      line.set_markeredgewidth(0.5)
-      line.set_markersize(6.0)
+  num_team = len(team_list)
 
-  # Set the line style of non-playoff teams to dotted
-  for line in line_array[8:]:
+  # Set line properties
+  for i in range(num_team):
+    line = line_array[i]
+    team = team_list[i]
+    colours = TEAM_COLOURS[team]
+
+    line.set_label(team)
+    line.set_color(colours['line'])
+    line.set_marker('.')
+    line.set_markerfacecolor(colours['marker'])
+    line.set_markeredgecolor(colours['edge'])
+    line.set_markeredgewidth(0.25)
+    line.set_markersize(7.5)
+
+    # Set the line style of non-playoff teams to dotted
+    if i >= 8:
       line.set_linestyle(':')
 
   axes.legend(ncol=PLOT['Legend']['NumCol'])
