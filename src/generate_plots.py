4becb4ed9dd63e7f10a3bfc1597e4c8ece2effe8
Stephen Young <s.young@riventa.com>
2020-10-03 17:57:33 -0400

    Update calls to functions from refactoring

--- src/generate_plots.py
+++ src/generate_plots.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import win_percent as wp
+import standings_progression as sp
 
 
 DATA_DIR = Path('./standings_data')
@@ -10,14 +10,14 @@ IMG_FORMAT = '.png'
 data_files = sorted(DATA_DIR.glob(DATA_FILE_PATTERN))
 
 for file in data_files:
-  
+
   try:
-    ws_data = wp.get_standings_data_from_spreadsheet(str(file))
+    ws_data = sp.get_standings_data_from_spreadsheet(str(file))
   except:
     print("Something went wrong with " + str(file))
     continue
 
-  win_frac_data = wp.process_spreadsheet_data(ws_data)
-  fig = wp.plot_win_fraction_by_date(win_frac_data)
+  win_frac_data = sp.process_standings_data(ws_data)
+  fig = sp.plot_standings_progression(win_frac_data)
   out_file = OUTPUT_DIR / file.with_suffix(IMG_FORMAT).name
   fig.savefig(str(out_file), format=IMG_FORMAT[1:])
