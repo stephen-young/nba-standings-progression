a76181ffb5771c587e046a93417000da54802781
Stephen Young <sx4_young@laurentian.ca>
2022-04-19 10:43:50 -0400

    Fix package name

--- test/standings_progression_test.py
+++ test/standings_progression_test.py
@@ -1,6 +1,8 @@
-"""MAIN.PY is a python script used for testing module functions"""
-import standings_progression as sp
+"""standings_progression_test.py is a python script used for testing module functions"""
+import nba_standings_progression.core as sp
 import matplotlib.pyplot as plt
+
+# TODO make script a proper test for use with a testing framework (tox + pytest?)
 
 filename = "./standings_data/2017_east_standings_by_date.xlsx"
 ws_data = sp.get_standings_data_from_spreadsheet(filename)
