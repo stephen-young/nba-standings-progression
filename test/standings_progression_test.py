a71bd34d04e371c43bdaa34f02cb924493236418
Stephen Young <sx4_young@laurentian.ca>
2022-05-04 11:35:58 -0400

    Rewrite test to work with tox and pytest

--- test/standings_progression_test.py
+++ test/standings_progression_test.py
@@ -1,12 +1,9 @@
 """standings_progression_test.py is a python script used for testing module functions"""
-import nba_standings_progression.core as sp
+import nba_standings_progression.nba_standings_progression as sp
 import matplotlib.pyplot as plt
+import pandas as pd
 
-# TODO make script a proper test for use with a testing framework (tox + pytest?)
-
-filename = "./standings_data/2017_east_standings_by_date.xlsx"
-ws_data = sp.get_standings_data_from_spreadsheet(filename)
-win_frac_data = sp.process_standings_data(ws_data)
-
-fig = sp.plot_standings_progression(win_frac_data)
-plt.show()
+def test_standings_data_from_web_return_type():
+    URL = "https://www.basketball-reference.com/leagues/NBA_2019_standings_by_date_eastern_conference.html"
+    data = sp.get_standings_data_from_web(URL)
+    assert type(data) is pd.DataFrame
