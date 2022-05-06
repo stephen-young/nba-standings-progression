9ba9879ac2e62c8497e1269d1a33dd6f175e112d
Stephen Young <sx4_young@laurentian.ca>
2022-05-05 21:47:15 -0400

    Add standings data test fixture
    Scrapping standings-by-date data from basketball reference is moved to a
    pytest fixture so that it will occur once for all tests. This limits the
    number of HTTP requests to the basketball reference website to once per
    test execution per tox test environment to be kind to the basketball
    reference servers.

--- test/standings_progression_test.py
+++ test/standings_progression_test.py
@@ -2,8 +2,13 @@
 import nba_standings_progression.nba_standings_progression as sp
 import matplotlib.pyplot as plt
 import pandas as pd
+import pytest
 
-def test_standings_data_from_web_return_type():
+@pytest.fixture(scope="session")
+def standings_data():
     URL = "https://www.basketball-reference.com/leagues/NBA_2019_standings_by_date_eastern_conference.html"
     data = sp.get_standings_data_from_web(URL)
-    assert type(data) is pd.DataFrame
+    return data
+
+def test_standings_data_from_web_return_type(standings_data):
+    assert type(standings_data) is pd.DataFrame
